a5de888b4d3a7493e4434312dcf90e29a6706b89
AidanV <aidanvanduyne@gmail.com>
2023-10-28 16:15:46 -0700

    reversed ranking and fixed search bug

--- src/main.rs
+++ src/main.rs
@@ -13,39 +13,45 @@ fn main() {
         .collect();
 
     loop {
-        println!("Enter a movie (q to quit):");
+        println!("Enter a movie (enter empty string to quit):");
         let mut movie = String::new();
         io::stdin()
             .read_line(&mut movie)
             .expect("Failed to read line");
         let movie: String = movie.trim().to_string();
-        if movie == "q" {
+        if movie == "" {
             break;
         }
-        let mut left: usize = 0;
-        let mut right: usize = movies.len() - 1;
-        let mut target: usize = 0;
-        while left <= right {
-            target = left + (right - left) / 2;
-            // println!("left {left} right {right} target {target}");
-            let movie_compare = movies[target].clone();
-            println!("Is {movie} better than {movie_compare} (yes or no):");
-            let mut is_better = String::new();
-            io::stdin()
-                .read_line(&mut is_better)
-                .expect("Failed to read line");
+        if movies.len() == 0 { 
+            movies.push(movie);
+        } else {
+            let mut left: usize = 0;
+            let mut right: usize = movies.len() - 1;
+            let mut target: usize = 0;
 
-            let is_better: String = is_better.trim().to_lowercase();
-            if right == left {
-                left += 1;
-                target += if is_better == "yes" { 1 } else { 0 };
-            } else if is_better == "yes" {
-                left = target + 1;
-            } else {
-                right = target;
+            while left <= right {
+                target = left + (right - left) / 2;
+                // println!("left {left} right {right} target {target}");
+                let movie_compare = movies[target].clone();
+                println!("Is {movie} better than {movie_compare} (yes or no):");
+                let mut is_better = String::new();
+                io::stdin()
+                    .read_line(&mut is_better)
+                    .expect("Failed to read line");
+
+                let is_better: String = is_better.trim().to_lowercase();
+                if right == left {
+                    left += 1;
+                    target += if is_better == "yes" { 1 } else { 0 };
+                } else if is_better == "yes" {
+                    left = target + 1;
+                } else {
+                    right = target;
+                    if left == right { break }
+                }
             }
+            movies.insert(movies.len() - target, movie);
         }
-        movies.insert(target, movie);
 
         fs::write("ranking.txt", movies.join("\n")).expect("failed to write");
 
