3a2316115817c189ebc7de3160c9f478f7c1171a
AidanV <aidanvanduyne@gmail.com>
2023-10-29 15:19:55 -0700

    working leptos

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,69 @@
-use std::fs;
-use std::io;
+#[cfg(feature = "ssr")]
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_files::Files;
+    use actix_web::*;
+    use leptos::*;
+    use leptos_actix::{generate_route_list, LeptosRoutes};
+    use ranker::app::*;
 
-fn main() {
+    let conf = get_configuration(None).await.unwrap();
+    let addr = conf.leptos_options.site_addr;
+    // Generate the list of routes in your Leptos App
+    let routes = generate_route_list(App);
+    println!("listening on http://{}", &addr);
 
-    let movies_file_as_string =
-        fs::read_to_string("ranking.txt").expect("Failed to read as string");
+    HttpServer::new(move || {
+        let leptos_options = &conf.leptos_options;
+        let site_root = &leptos_options.site_root;
 
-    let mut movies: Vec<String> = movies_file_as_string
-        .split('\n')
-        .map(|elem| elem.to_string())
-        .filter(|elem| elem != "")
-        .collect();
+        App::new()
+            .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
+            // serve JS/WASM/CSS from `pkg`
+            .service(Files::new("/pkg", format!("{site_root}/pkg")))
+            // serve other assets from the `assets` directory
+            .service(Files::new("/assets", site_root))
+            // serve the favicon from /favicon.ico
+            .service(favicon)
+            .leptos_routes(leptos_options.to_owned(), routes.to_owned(), App)
+            .app_data(web::Data::new(leptos_options.to_owned()))
+        //.wrap(middleware::Compress::default())
+    })
+    .bind(&addr)?
+    .run()
+    .await
+}
 
-    loop {
-        println!("Enter a movie (enter empty string to quit):");
-        let mut movie = String::new();
-        io::stdin()
-            .read_line(&mut movie)
-            .expect("Failed to read line");
-        let movie: String = movie.trim().to_string();
-        if movie == "" {
-            break;
-        }
-        if movies.len() == 0 { 
-            movies.push(movie);
-        } else {
-            let mut left: usize = 0;
-            let mut right: usize = movies.len() - 1;
-            let mut target: usize = 0;
+#[cfg(feature = "ssr")]
+#[actix_web::get("favicon.ico")]
+async fn favicon(
+    leptos_options: actix_web::web::Data<leptos::LeptosOptions>,
+) -> actix_web::Result<actix_files::NamedFile> {
+    let leptos_options = leptos_options.into_inner();
+    let site_root = &leptos_options.site_root;
+    Ok(actix_files::NamedFile::open(format!(
+        "{site_root}/favicon.ico"
+    ))?)
+}
 
-            while left <= right {
-                target = left + (right - left) / 2;
-                // println!("left {left} right {right} target {target}");
-                let movie_compare = movies[target].clone();
-                println!("Is {movie} better than {movie_compare} (yes or no):");
-                let mut is_better = String::new();
-                io::stdin()
-                    .read_line(&mut is_better)
-                    .expect("Failed to read line");
+#[cfg(not(any(feature = "ssr", feature = "csr")))]
+pub fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for pure client-side testing
+    // see lib.rs for hydration function instead
+    // see optional feature `csr` instead
+}
 
-                let is_better: String = is_better.trim().to_lowercase();
-                if right == left {
-                    left += 1;
-                    target += if is_better == "yes" { 1 } else { 0 };
-                } else if is_better == "yes" {
-                    left = target + 1;
-                } else {
-                    right = target;
-                    if left == right { break }
-                }
-            }
-            movies.insert(movies.len() - target, movie);
-        }
+#[cfg(all(not(feature = "ssr"), feature = "csr"))]
+pub fn main() {
+    // a client-side main function is required for using `trunk serve`
+    // prefer using `cargo leptos serve` instead
+    // to run: `trunk serve --open --features csr`
+    use leptos::*;
+    use ranker::app::*;
+    use wasm_bindgen::prelude::wasm_bindgen;
 
-        fs::write("ranking.txt", movies.join("\n")).expect("failed to write");
+    console_error_panic_hook::set_once();
 
-        println!("{movies:?}");
-    }
+    leptos::mount_to_body(App);
 }
